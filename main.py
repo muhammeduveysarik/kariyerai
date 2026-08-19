d393b6ec23fb8eaddc159db74c2b5303e88fbe06
MUHAMMED ÜVEYS ARIK <muveysarik6@gmail.com>
2026-08-19 20:32:06 +0300

    Fix CORS for Vercel frontend

--- main.py
+++ main.py
@@ -18,7 +18,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://kariyerai.vercel.app",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000"
+    ],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
